0d6bac654ce89b7a143ac45ff1236244bf115006
Sophie Popow <sophie.popow@gmail.com>
2020-07-12 21:24:54 +0200

    adding segues + minor fixes

---
+++ SwooshApp/Controller/SkillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SkillViewController.swift
+//  SwooshApp
+//
+//  Created by Sophie Popow on 11/07/2020.
+//  Copyright © 2020 Sophie Popow. All rights reserved.
+//
+
+import UIKit
+
+class SkillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
